881a452ce167afd5348609478c8844db536bc714
Ketan kirty <115186402+ketankirty@users.noreply.github.com>
2025-09-19 14:53:48 +0530

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -44,11 +44,10 @@ app.use((req, res, next) => {
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected successfully'))
+  .then(() => console.log('MongoDB connected successfully'))
 .catch(err => console.error('MongoDB connection error:', err));
+})
+
 
 // Routes
 app.use('/api/auth', authRoutes);
