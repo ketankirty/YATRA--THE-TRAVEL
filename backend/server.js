9681db5c79ed52ab2c81d3bb8320fb01e39b4be5
Ketan kirty <115186402+ketankirty@users.noreply.github.com>
2025-09-20 19:29:57 +0530

    Fix Backend Server and API Connection Issues

--- backend/server.js
+++ backend/server.js
@@ -44,9 +44,9 @@ app.use((req, res, next) => {
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
-  .then(() => console.log('MongoDB connected successfully'))
-.catch(err => console.error('MongoDB connection error:', err));
 })
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 
 // Routes
